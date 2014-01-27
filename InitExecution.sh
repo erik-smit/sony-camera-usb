faa12362d39efb781537b34d14a0ef4e21a0ba21
erik-smit <smite@zylon.net>
2014-01-27 15:40:05 +0100

    More attempts

---
+++ InitExecution.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+DEV=$1
+
+printf '\x00\x00\x00\x00\x00\x00\x01\x00' | sg_raw --nosense -s 8 $DEV 7A 00 00 00 00 00 00 00 00 00 00 00
+sg_raw --nosense -r 64 $DEV 7A 00 00 00 00 00 00 00 00 00 00 00
+
