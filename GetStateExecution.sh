74fc9e6399b0631640b00c92adf5c7055c08bc13
erik-smit <smite@zylon.net>
2015-04-07 10:33:07 -0400

    usb 1-2: New USB device found, idVendor=054c, idProduct=06a8

--- GetStateExecution.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-DEV=$1
-
-printf '\x00\x00\x00\x00\x00\x00\x00\x02' | sg_raw --nosense -s 8 $DEV 7A 00 00 00 00 00 00 00 00 00 00 00
-sg_raw --nosense -r 64 $DEV 7A 00 00 00 00 00 00 00 00 00 00 00
-
--- SwitchMode.sh
+++ SwitchMode.sh
@@ -2,6 +2,6 @@
 
 DEV=$1
 
-printf '\x00\x00\x00\x00\x00\x00\x30\x00' | sg_raw --nosense -s 8 $DEV 7A 00 00 00 00 00 00 00 00 00 00 00
-sg_raw --nosense -r 64 $DEV 7A 00 00 00 00 00 00 00 00 00 00 00 
+printf '\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00' | sg_raw --nosense -s 8 $DEV 7A 05 00 00 00 00 00 00 00 00 00 00
+sg_raw --nosense -r 64 $DEV 7A 05 00 00 00 00 00 00 00 00 00 00
 
